7bd44a177a7a712f3349fcff31715b8785585199
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-01-24 14:31:34 -0500

    Add template code, add queries for home page and additem

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql      = require('mysql');
-
-const config = require('../config ');
+var mysql = require('mysql');
+const config = require('../config');
 console.log(config); 
 var connection = mysql.createConnection(config);
-
 connection.connect();
 
 console.log("I connected!!!")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const selectQuery = 'SELECT * FROM tasks;';
+  connection.query(selectQuery,(error,results)=>{
+    res.render('index', { taskArray: results });
+  })
 });
+
+router.post('/addItem',(req, res, next)=>{
+  const newTask = req.body.newTask;
+  const newTaskDate = req.body.newTaskDate;
+  // We know what the user submitted in the FormData. It comes to this route
+  // inside req.body.NAMEOFFIELD. We store those values into a var.
+  // Now we take those vars, and insert them into MySQL.
+
+  // THIS IS BAD!!!!!!!!
+  const badInsertQuery = `INSERT INTO tasks (taskName,taskDate)
+             VALUES
+          (${newTask},${newTaskDate});`;
+
+	// var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES ('"+newTask+"','"+dueDate+"')"
+  // SQL injection.
+  // is when a user inserts a SQL statement into a form
+  // to run SQL when the dev didn't intend.
+  const insertQuery = `INSERT into tasks(taskName,taskDate)
+    VALUES
+    (?,?);`;
+  console.log(insertQuery);
+
+  connection.query(insertQuery,[newTask,newTaskDate], (error, results)=>{
+    if(error){
+      // something bad happened. STOP
+      throw error;
+    }else{
+      // Query succeeeded. Forward user to homepage
+      res.redirect('/');
+    }
+  })
+
+  // res.json(req.body)
+})
+
 
 module.exports = router;
