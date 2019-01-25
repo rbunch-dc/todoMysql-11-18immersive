3a640734dc146018856a561a02438eff6e6dbb02
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-01-25 15:05:19 -0500

    Add update and update.ejs

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,9 @@ console.log("I connected!!!")
 router.get('/', function(req, res, next) {
   const selectQuery = 'SELECT * FROM tasks;';
   connection.query(selectQuery,(error,results)=>{
-    res.render('index', { taskArray: results });
+    res.render('index', { 
+      taskArray: results 
+    });
   })
 });
 
@@ -51,5 +53,57 @@ router.post('/addItem',(req, res, next)=>{
   // res.json(req.body)
 })
 
+// a wildcard route in Express 
+// has a : in front of it
+router.get('/delete/:id',(req,res,next)=>{
+  // wildcard routes are available in req.params
+  // res.json(req.params);
+  const deleteQuery = `DELETE FROM tasks WHERE id = ?;`;
+  connection.query(deleteQuery,[req.params.id],(error)=>{
+    res.redirect('/');
+  })
+
+})
+
+router.get('/edit/:id',(req, res)=>{
+  // this route should be a pre-populated form
+  // with the element with this id values
+  const selectQuery = `SELECT * FROM tasks
+    WHERE id = ?;`;
+  connection.query(selectQuery,[req.params.id],(err,results)=>{
+    results[0].taskDate  = formatDate(results[0].taskDate);
+    res.render('edit',{
+      task: results[0],
+    });
+  });
+});
+
+router.post('/editItem',(req, res)=>{
+  // UPDATE the db
+  const updateQuery = `UPDATE tasks SET 
+    taskName = ?,
+    taskDate = ?
+    WHERE id = ?`
+  connection.query(updateQuery,[req.body.newTask, req.body.newTaskDate, req.body.taskId],(error, results)=>{
+    if(error){
+      throw error;
+    }else{
+      res.redirect('/');
+    }
+  })
+})
 
 module.exports = router;
+
+
+function formatDate(date) {
+  let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
